e1e85170a6668663a7d64e409015f17577ff1d98
yjx <yangjx009@139.com>
2019-11-19 17:41:09 +0800

    add new

--- stock/index.py
+++ stock/index.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     for index, row in data.iterrows():
         curIdx += 1
         print(row['ts_code'], row['symbol'])
-        df = pro.daily(ts_code=row['ts_code'], start_date='20191114', end_date='20191115')
+        df = pro.daily(ts_code=row['ts_code'], start_date='20191118', end_date='20191119')
         #print(df)
         ls = []
         for index2, row2 in df.iterrows():
